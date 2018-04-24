48f5cb817c4480638c74beb119f76f0e00659fe8
WenChao Kong <wenchao.kong@auspost.com.au>
2018-04-24 19:34:45 +1000

    convert to cradle project

--- src/main/java/network/HttpClient.kt
+++ src/main/java/network/HttpClient.kt
@@ -1,14 +1,14 @@
 package network
 
-import okhttp3.*
+import okhttp3.Credentials
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.IOException
-import java.security.cert.CertificateException
-import java.security.cert.X509Certificate
-import javax.net.ssl.*
 
 
 class HttpClient {
