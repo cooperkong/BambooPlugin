53da4fcc4a6a5721f5dfedd62307ba424288f91f
WenChao Kong <wenchao.kong@auspost.com.au>
2018-05-03 09:19:54 +1000

    remove plugin as an action, added as a tool window instead

---
+++ src/main/java/com/bamboo/plugin/BambooPluginToolWindow.kt
@@ -0,0 +1,19 @@
+package com.bamboo.plugin
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.ui.content.Content
+import com.intellij.ui.content.ContentFactory
+import ui.auth.LogInForm
+
+class BambooPluginToolWindow : ToolWindowFactory {
+    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        val content = ContentFactory.SERVICE.getInstance().createContent(LogInForm().show(), "", false)
+        toolWindow.contentManager.addContent(content)
+    }
+
+    override fun shouldBeAvailable(project: Project): Boolean {
+        return true
+    }
+}
